fccbc882c7ea1bd6a98b5169391162dad3f6351b
Artem <skyar7@yandex.ru>
2023-05-31 13:55:27 +0700

    initial

---
+++ Roulette_the_Game/Person.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+
+namespace Roulette_the_Game
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public byte Age { get; set; }
+        public int Cash { get; set; } = 1000;
+        public int Round { get; set; } = 1;
+        public int MaxNum { get; set; } = 5;
+        public int Coef { get; set; } = 4;
+        public int Bet { get; set; }
+        public bool IsGod { get; set; } = false;
+
+        // Метод получения результата.
+        public void IsWin(int playerchoise)
+        {
+            var posWin = Bet * Coef;
+            
+            // Режим бога.
+            if (IsGod)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("БЫЛ ВВЕДЁН ЧИТ-КОД!");
+                Console.WriteLine("АКТИВИРОВАН РЕЖИМ БОГА!");
+                Console.WriteLine(Name + " - РУЛЕТОЧНЫЙ БОГ!");
+                Console.WriteLine();
+                Console.WriteLine("На вашем счету: " + Cash);
+                Console.WriteLine("Ваша ставка: " + Bet);
+                Console.WriteLine("Вы поставили на: " + playerchoise);
+                Console.WriteLine("Пока ты моргнул, уже выпало: " + playerchoise);
+                Cash += posWin;
+                Console.WriteLine("У ВАС УДАЧА БОГА!");
+                Console.WriteLine("Ваш выигрыш: " + posWin);
+               
+            }
+            // Обычный режим игры.
+            else
+            {
+                // Заставка.
+                var loading = "=";
+                for (var i = 0; i < 43; i++)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("На вашем счету: " + Cash);
+                    Console.WriteLine("Ваша ставка: " + Bet);
+                    Console.WriteLine("Возможный выигрыш: " + posWin);
+                    Console.WriteLine("Вы поставили на: " + playerchoise);
+                    if (i % 2 == 0 && i != 42)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else if (i == 42)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    Console.WriteLine("Рулетка крутится, лавеха мутится (не точно)");
+                    Console.WriteLine(loading);
+                    loading = (loading + "=");
+                    Thread.Sleep(40);
+                }
+
+                // Проверка победы.
+                Random rnd = new Random();
+                var comeOut = rnd.Next(1, MaxNum);
+                if (playerchoise == comeOut)
+                {
+                    Cash += posWin;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Выпало: " + comeOut);
+                    Console.WriteLine("ВЫ ПОБЕДИЛИ!");
+                    Console.WriteLine("Ваш выигрыш: " + posWin);
+                }
+                else
+                {
+                    Cash -= Bet;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Выпало: " + comeOut);
+                    Console.WriteLine("ВЫ ПРОИГРАЛИ!");
+                }
+            }
+           
+        }
+    }
+}
---
+++ Roulette_the_Game/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace Roulette_the_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person player = new Person();
+            
+            // Ввода имени игрока и проверка.
+            var isCorrectName = false;
+            do
+            {
+                Console.WriteLine("Введите ваше имя:");
+                string inputname = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(inputname))
+                {
+                    Console.WriteLine("Нельзя оставить поле пустым! Введите как к вам обращаться:");
+                }
+                else
+                {
+                    isCorrectName = true;
+                    player.Name = inputname;
+                }
+            } while (isCorrectName == false);
+
+            // Ввод возраста игрока и проверка.
+            byte inputAge;
+            Console.WriteLine("Введите сколько вам лет:");
+            while (!byte.TryParse(Console.ReadLine(), out inputAge))
+            {
+                Console.WriteLine("Ошибка ввода! Введите сколько вам полных лет:");
+            }
+            player.Age = inputAge;
+            if (player.Age < 5)
+            {
+                Console.Clear();
+                Console.WriteLine("НЕВОЗМОЖНО! ТЫ СЛИШКОМ МАЛЕНЬКИЙ И ТУПОЙ");
+                Console.WriteLine();
+                Console.WriteLine("Нажми любую клавишу для выхода");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+   
+            // Проверка ввода чит-кода для средств на счету.
+            if (player.Name.Equals("Sum") && player.Age == 41)
+            {
+                Console.Clear();
+                Console.WriteLine($"ПОЗДРАВЛЯЕМ, {player.Name + player.Age}! ВЫ ВВЕЛИ ЧИТ-КОД!");
+                Console.WriteLine("Теперь вы сказачно богаты!");
+                player.Cash = 9999999;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine($"Привет, {player.Name}! Давай сыграем в рулетку!");
+            }
+
+            // Игровой цикл.
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Раунд " + player.Round);
+                Console.WriteLine("На счету: " + player.Cash);
+
+                // Ввод и проверка выбора.
+                Console.WriteLine($"На что ставите от 1 до {player.MaxNum}:");
+                var choise = 0;
+                var choiseCorrect = false;
+                while(!choiseCorrect)
+                    {
+                        while (!Int32.TryParse(Console.ReadLine(), out choise))
+                        {
+                            Console.WriteLine("Ошибка ввода! Введите число от 1 до 5: ");
+                        }
+                        if (choise < 1 || choise > 5)
+                        {
+                            Console.WriteLine("Ошибка ввода! Введите число от 1 до 5: ");
+                        }
+                        else
+                        {
+                        choiseCorrect = true;
+                        }
+                    }
+
+                // Ввод и проверка ставки.
+                Console.WriteLine($"Сколько ставите (возможный выигрыш {player.Coef}Х): ");
+                var bet = 0;
+                var betCorrect = false;
+                while (!betCorrect)
+                {
+                    while (!Int32.TryParse(Console.ReadLine(), out bet))
+                    {
+                        Console.WriteLine("Ошибка ввода! Введите сколько вы хотите поставить: ");
+                    }
+                    if (bet < 1)
+                    {
+                        Console.WriteLine("Ставка должна быть больше нуля. Попробуйте ещё раз: ");
+                    }
+                    else if (bet > player.Cash)
+                    {
+                        Console.WriteLine("У вас недостаточно средств на счету. Попробуйте ещё раз: ");
+                    }
+                    else
+                    {
+                        betCorrect = true;
+                    }
+                }
+                player.Bet = bet;
+                
+                // Проверка на режим бога.
+                if (player.Bet == 777)
+                {
+                    player.IsGod = true;
+                }
+                else if (player.Bet == 666)
+                {
+                    player.IsGod = false;
+                }
+                
+                // Результат игры.
+                player.IsWin(choise);
+                player.Round++;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("===========================================");
+            } while (player.Cash >= 1);
+            Console.WriteLine();
+            Console.WriteLine($"У вас, {player.Name}, кончились средства на счету! Очень жаль (нет)");
+            Console.WriteLine("ИГРА ОКОНЧЕНА.");
+            Console.WriteLine();
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.ReadKey();
+        }
+    }
+}
